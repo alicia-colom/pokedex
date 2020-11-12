22a672f6298f4158c010b780a85e979a2d9bbf29
alicia-colom <aliciacolom@gmail.com>
2020-11-12 11:21:35 +0100

    Add styles in improvements branch

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ import PokeList from './PokeList';
 class App extends Component {
 	render() {
 		return (
-			<div className="app">App
+			<div className="app">
+				<img className="logo" src="../images/logo-pokemon.png" />
 				<PokeList pokedex={pokedex} />
 			</div>
 		);
